762a65c8ebaf0488da4fc57792f2c2a9f1c64b31
Alex Homer <realalexhomer@users.noreply.github.com>
2016-04-12 00:27:56 -0400

    Create constants.babel.js

---
+++ constants.babel.js
@@ -0,0 +1,13 @@
+const CharacterIds = ['1', '4', '6', '2'];
+
+const DaysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+const StaticURL = "https://swapi.co/api/";
